112a7885deb50df2ce17c1136d79b1e905abf05c
Jeff Ananias <jananias@fastmail.com>
2024-10-21 16:36:14 -0700

    Started front page

---
+++ src/Search.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+
+interface SearchProps {
+  placeholder: string;
+  onSubmit: (searchTerm: string) => void;
+};
+
+const Search: React.FC<SearchProps> = function ({ items }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        // type="search" 
+        // placeholder={placeholder}
+        // value={searchTerm}
+        // onChange={handleSearch}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default Search;
